642388b1df096d220add3459d3361ae421390bd2
Julius Lassen <julius5517@gmail.com>
2023-09-25 13:13:45 +0200

    created structure in project

---
+++ src/main/java/org/dat3/config/HibernateConfig.java
@@ -0,0 +1,68 @@
+package org.dat3.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import lombok.NoArgsConstructor;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
+
+
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
+public class HibernateConfig {
+
+    private static EntityManagerFactory entityManagerFactory;
+    private static String dbName;
+
+    private static EntityManagerFactory buildEntityFactoryConfig() {
+        try {
+            Configuration configuration = new Configuration();
+
+            Properties props = new Properties();
+            String connctionURL = String.format("jdbc:postgresql://localhost:5432/%s?currentSchema=public", dbName);
+            props.put("hibernate.connection.url", connctionURL);
+            props.put("hibernate.connection.username", "postgres");
+            props.put("hibernate.connection.password", "postgres");
+            props.put("hibernate.show_sql", "true"); // show sql in console
+            props.put("hibernate.format_sql", "true"); // format sql in console
+            props.put("hibernate.use_sql_comments", "true"); // show sql comments in console
+
+            props.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect"); // dialect for postgresql
+            props.put("hibernate.connection.driver_class", "org.postgresql.Driver"); // driver class for postgresql
+            props.put("hibernate.archive.autodetection", "class"); // hibernate scans for annotated classes
+            props.put("hibernate.current_session_context_class", "thread"); // hibernate current session context
+            props.put("hibernate.hbm2ddl.auto", "update"); // hibernate creates tables based on entities
+
+
+            return getEntityManagerFactory(configuration, props);
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    private static EntityManagerFactory getEntityManagerFactory(Configuration configuration, Properties props) {
+        configuration.setProperties(props);
+
+        getAnnotationConfiguration(configuration);
+
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+        System.out.println("Hibernate Java Config serviceRegistry created");
+
+        SessionFactory sf = configuration.buildSessionFactory(serviceRegistry);
+        return sf.unwrap(EntityManagerFactory.class);
+    }
+
+    private static void getAnnotationConfiguration(Configuration configuration) {
+        // add annotated classes
+
+    }
+
+    public static EntityManagerFactory getEntityManagerFactoryConfig(String name) {
+        dbName = name;
+        if (entityManagerFactory == null) entityManagerFactory = buildEntityFactoryConfig();
+        return entityManagerFactory;
+    }
+}
