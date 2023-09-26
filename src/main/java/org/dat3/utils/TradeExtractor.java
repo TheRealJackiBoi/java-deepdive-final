bd2762a2a036bcc06b46c6987b8ab1a1e6cc8c87
artiecodes <bjarke.dehlie@gmail.com>
2023-09-26 15:13:01 +0200

    TradeExtractor almost done, needs currency argument fixed

--- src/main/java/org/dat3/utils/TradeExtractor.java
+++ src/main/java/org/dat3/utils/TradeExtractor.java
@@ -5,44 +5,45 @@ import org.dat3.model.Value;
 import org.jsoup.select.Elements;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class TradeExtractor {
 
     public static List<Value> extractData(Elements elements) {
-        // check om currency eksisiter, ellers opret en
+
+        // check om currency eksisterer, ellers opret en
         // returner en value
 
+
         //EntityManagerFactory emf = HibernateConfig.createEntityManagerFactoryConfig("DBNAME");
-/*
+
         List<Value> values = elements.stream()
                 .map(element -> {
-                    Value value = new Value(
-                            // Current value of currency
-                            Double.parseDouble(element.select("div.currencyItem_actualValueContainer__2xLkB").text().replace(",", ".")),
-                            // Time at the date of retrieval
-                            LocalDateTime.now(),
-                            // Currency
-                            null);
-                    return value;
+                    // Fetches values from string and replaces comma with dot to parse properly to double
+                    String text = element.select("div.currencyItem_actualValueContainer__2xLkB").text().replace(",", ".");
+
+                    // Creates an array of the strings to make it possible to parse to double by removing the space
+                    String[] separateDoubles = text.split(" ");
+
+
+
+                    // Creates a separate object for each value
+                    List<Value> subValues = Arrays.stream(separateDoubles)
+                            .map(doubleValue -> new Value(
+                                    Double.parseDouble(doubleValue),
+                                    LocalDateTime.now(),
+                                    //LocalDateTime.parse(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"))),
+                                    null))
+                            .collect(Collectors.toList());
+
+                    return subValues;
                 })
-                .toList();
-        */
-
-        List<Value> values = elements.stream()
-                .forEach(element -> {
-                    Value value = new Value(
-                            // Current value of currency
-                            Double.parseDouble(element.select("div.currencyItem_actualValueContainer__2xLkB").text().replace(",", ".")),
-                            // Time at the date of retrieval
-                            LocalDateTime.now(),
-                            // Currency
-                            null);
-                    return element;
-                });
-
-
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
 
         return values;
     }
