9137424e3cf9ad2f5da92b627d2342f801ed144c
TheRealJackiBoi <jslam@oulund.dk>
2023-09-26 13:31:59 +0200

    CountryExtractor done and some updates to model🪄

--- src/main/java/org/dat3/utils/CountryExtractor.java
+++ src/main/java/org/dat3/utils/CountryExtractor.java
@@ -1,22 +1,55 @@
 package org.dat3.utils;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
+import org.dat3.dao.CountryDAO;
+import org.dat3.dao.CurrencyDAO;
 import org.dat3.model.Country;
+import org.dat3.model.Currency;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CountryExtractor {
 
-        public static String extract(String jsonStr) {
-            String result = null;
+        public static List<Country> extract(String jsonStr) {
+            List<Country> countries = new ArrayList<>();
+
+            CurrencyDAO currencyDAO = CurrencyDAO.getInstance();
+            CountryDAO countryDAO = CountryDAO.getInstance();
+
             try {
-                Country countries = gson.fromJson(jsonStr, Country.class);
-                //result = obj.getString("country");
+                JsonArray obj = gson.fromJson(jsonStr, JsonArray.class);
+
+                for (JsonElement jsonElement : obj) {
+                    JsonObject jObject = jsonElement.getAsJsonObject();
+
+                    //values
+                    String name = jObject.get("name").getAsJsonObject().get("common").getAsString();
+                    String capital = jObject.get("capital").getAsString();
+                    double area = jObject.get("area").getAsDouble();
+                    int population = jObject.get("population").getAsInt();
+                    String cca3 = jObject.get("cca3").getAsString();
+
+                    //currency
+                    JsonObject currencyJson = jObject.get("currency").getAsJsonObject();
+                    Currency currency = currencyDAO.findById(Currency.class, currencyJson.get("code").getAsString());
+
+                    if (currency == null) {
+                        currency = new Currency(currencyJson.get("code").getAsString(), currencyJson.get("name").getAsString());
+                        currencyDAO.create(currency);
+                    }
+
+                    //country
+                    Country country = new Country(name, capital, area, population, cca3);
+                    currency.addCountry(country);
+                    countryDAO.create(country);
+
+                    countries.add(country);
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            return result;
+            return countries;
         }
 
     static Gson gson = new GsonBuilder()
