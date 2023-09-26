ba34cae1ee1fd05c4f7e288448615a2b17ee98d8
artiecodes <bjarke.dehlie@gmail.com>
2023-09-26 16:03:07 +0200

    TradeExtractor working(?), not tested because of EMF issues

--- src/main/java/org/dat3/utils/TradeExtractor.java
+++ src/main/java/org/dat3/utils/TradeExtractor.java
@@ -1,6 +1,8 @@
 package org.dat3.utils;
 
 import jakarta.persistence.EntityManagerFactory;
+import org.dat3.dao.CurrencyDAO;
+import org.dat3.model.Currency;
 import org.dat3.model.Value;
 import org.jsoup.select.Elements;
 
@@ -21,23 +23,36 @@ public class TradeExtractor {
 
         //EntityManagerFactory emf = HibernateConfig.createEntityManagerFactoryConfig("DBNAME");
 
+        CurrencyDAO currencyDAO = CurrencyDAO.getInstance();
+
         List<Value> values = elements.stream()
                 .map(element -> {
                     // Fetches values from string and replaces comma with dot to parse properly to double
-                    String text = element.select("div.currencyItem_actualValueContainer__2xLkB").text().replace(",", ".");
+
+                    String doubles = element.select("div.currencyItem_actualValueContainer__2xLkB").text().replace(",", ".");
 
                     // Creates an array of the strings to make it possible to parse to double by removing the space
-                    String[] separateDoubles = text.split(" ");
+                    String[] separateDoubles = doubles.split(" ");
 
+                    // Fetches currency codes
+                    String codes = element.select("div.currencyItem_currencySymbolContainer__RKWSP").text();
 
+                    // Fetches Danish names of currencies
+                    String names = element.select("div.currencyItem_currencyNameContainer__19YHn").text();
+
+                    // Fetches percentage change in currency ready to be parsed to double if needed
+                    String change = element.select("div.currencyItem_currencyChangeContainer__pV3ni").text().replace(",", ".");
+                    String[] separateChange = change.split(" ");
 
                     // Creates a separate object for each value
                     List<Value> subValues = Arrays.stream(separateDoubles)
-                            .map(doubleValue -> new Value(
+                            .map(doubleValue -> {
+                                Currency currency = currencyDAO.findById(Currency.class, codes);
+                                return new Value(
                                     Double.parseDouble(doubleValue),
                                     LocalDateTime.now(),
-                                    //LocalDateTime.parse(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"))),
-                                    null))
+                                    currency);
+                            })
                             .collect(Collectors.toList());
 
                     return subValues;
