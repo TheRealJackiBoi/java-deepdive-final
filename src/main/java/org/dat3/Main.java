73daa8a1771ec7613ad276d9be7cc90e92c6cac6
Jack <33868006+TheRealJackiBoi@users.noreply.github.com>
2023-09-26 13:53:51 +0200

    Update Main.java

--- src/main/java/org/dat3/Main.java
+++ src/main/java/org/dat3/Main.java
@@ -4,10 +4,8 @@ import org.dat3.utils.CountryFetcher;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bjaaark er iron 2!");
 
         String countryApiUrl = "https://restcountries.com/v3.1/currency/";
 
-        System.out.println(CountryFetcher.fetch(countryApiUrl, "EUR"));
     }
 }
