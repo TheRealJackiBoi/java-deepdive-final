c41048b394ffbc1b24eb9dcbb87d148a5d58699b
asphixia <william2450>
2023-09-28 13:06:07 +0200

    dont need to push

--- src/main/java/org/dat3/Main.java
+++ src/main/java/org/dat3/Main.java
@@ -1,11 +1,20 @@
 package org.dat3;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.dat3.config.HibernateConfig;
 import org.dat3.utils.CountryFetcher;
 
 public class Main {
     public static void main(String[] args) {
 
         String countryApiUrl = "https://restcountries.com/v3.1/currency/";
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactoryConfig("currencyDatabase");
+        try (EntityManager em = emf.createEntityManager()){
+            em.getTransaction().begin();
+            em.getTransaction().commit();
+
+        }
 
     }
 }
