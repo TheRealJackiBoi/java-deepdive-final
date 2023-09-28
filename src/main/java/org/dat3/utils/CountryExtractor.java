b7f595e2bff7f7c949be173c466778a2c68724c3
asphixia <william2450>
2023-09-28 17:16:17 +0200

    made thread on CountryExtractor

--- src/main/java/org/dat3/utils/CountryExtractor.java
+++ src/main/java/org/dat3/utils/CountryExtractor.java
@@ -1,6 +1,7 @@
 package org.dat3.utils;
 
 import com.google.gson.*;
+import org.dat3.config.ExecutorServiceConfig;
 import org.dat3.dao.CountryDAO;
 import org.dat3.dao.CurrencyDAO;
 import org.dat3.model.Country;
@@ -8,6 +9,9 @@ import org.dat3.model.Currency;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 public class CountryExtractor {
 
@@ -17,51 +21,66 @@ public class CountryExtractor {
             CurrencyDAO currencyDAO = CurrencyDAO.getInstance();
             CountryDAO countryDAO = CountryDAO.getInstance();
 
-            Currency currency = null;
+            //instantiante ExecutorService
+            ExecutorServiceConfig executorServiceConfig = new ExecutorServiceConfig();
+            ExecutorService executorService = executorServiceConfig.getExecutorService();
 
+            //moved the currency to a global variable
+            //Currency currency = null;
             try {
                 JsonArray obj = gson.fromJson(jsonStr, JsonArray.class);
+                List<Callable<Country>> taskList = new ArrayList<>();
 
                 for (JsonElement jsonElement : obj) {
+                    taskList.add(() -> {
 
-                    JsonObject jObject = jsonElement.getAsJsonObject();
+                        JsonObject jObject = jsonElement.getAsJsonObject();
 
-                    //values
-                    String name = jObject.get("name").getAsJsonObject().get("common").getAsString();
+                        //values
+                        String name = jObject.get("name").getAsJsonObject().get("common").getAsString();
 
-                    //Check if country already exists
-                    if (countryDAO.findById(Country.class, name) != null) {
-                        continue;
-                    }
+                        //Check if country already exists
+                        if (countryDAO.findById(Country.class, name) != null) {
+                            return null;
+                            //continue dont work
+                            //continue;
+                        }
 
-                    String capital = jObject.get("capital").getAsString();
-                    double area = jObject.get("area").getAsDouble();
-                    int population = jObject.get("population").getAsInt();
-                    String cca3 = jObject.get("cca3").getAsString();
+                        String capital = jObject.get("capital").getAsString();
+                        double area = jObject.get("area").getAsDouble();
+                        int population = jObject.get("population").getAsInt();
+                        String cca3 = jObject.get("cca3").getAsString();
 
-                    //currency
-                    JsonObject currencyJson = jObject.get("currency").getAsJsonObject();
+                        //currency
+                        JsonObject currencyJson = jObject.get("currency").getAsJsonObject();
 
-                    //Check if currency already exists
-                    if (currency == null) {
-                        currency = currencyDAO.findById(Currency.class, currencyJson.get("code").getAsString());
-                        if (currency != null) {
+                        //Check if currency already exists
+                        Currency currency = null;
+                        if (currency == null) {
+                            currency = currencyDAO.findById(Currency.class, currencyJson.get("code").getAsString());
+                            if (currency != null) {
+                                currency = new Currency(currencyJson.get("code").getAsString(), currencyJson.get("name").getAsString());
+                                currencyDAO.create(currency);
+                            }
+                        } else if (currency.getCode() != currencyJson.get("code").getAsString()) {
                             currency = new Currency(currencyJson.get("code").getAsString(), currencyJson.get("name").getAsString());
                             currencyDAO.create(currency);
                         }
+
+                        //country
+                        Country country = new Country(name, capital, area, population, cca3);
+                        currency.addCountry(country);
+                        countryDAO.create(country);
+
+                        return country;
+                    });
+                }
+                List<Future<Country>> futures = executorService.invokeAll(taskList);
+                for (Future<Country> future : futures) {
+                    Country country = future.get();
+                    if (country != null) {
+                        countries.add(country);
                     }
-                    else if (currency.getCode() != currencyJson.get("code").getAsString()) {
-                        currency = new Currency(currencyJson.get("code").getAsString(), currencyJson.get("name").getAsString());
-                        currencyDAO.create(currency);
-                    }
-
-                    //country
-                    Country country = new Country(name, capital, area, population, cca3);
-                    currency.addCountry(country);
-                    countryDAO.create(country);
-
-                    countries.add(country);
-
                 }
 
             } catch (Exception e) {
